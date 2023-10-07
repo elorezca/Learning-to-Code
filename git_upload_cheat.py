01f886c07cc83fc975391d88ae43d23df824eb29
elorezca <elorezca@gmail.com>
2023-10-07 18:22:44 -0400

    Basic Calculator

---
+++ git_upload_cheat.py
@@ -0,0 +1,5 @@
+# git add . 
+
+# git commit -m "YOUR COMMIT MESSAGE" 
+
+# git push 
---
+++ shapes.py
@@ -0,0 +1,50 @@
+import math
+
+
+print("")
+
+print("We will calculate the area of a retangle")
+
+print("")
+
+x = float(input("What is the length of the rentangle? "))
+
+print("")
+
+y = float(input("What is the width of the retangle? "))
+
+print("")
+
+print(f"The area of the retangles is {x*y}")
+
+print("")
+
+print("Next we will calculate the area of a circle")
+
+print("")
+
+a = float(input("What is the radius? "))
+
+print("")
+
+b = a**2 * (math.pi)
+
+print(f"The aare of the circle with a radius of {a} is {b}")
+
+print("")
+
+print("We will calculate the area of a triangle")
+
+print("")
+
+c = float(input("What is the Height? "))
+
+print("")
+
+d = float(input("What is the Base? "))
+
+e = 0.5 * c * d
+
+print("")
+
+print(f"The are of the triangle with a height of {c} and a base of {d} is {e}")
