6b65a5d88f5618b50ee7a9913d5aacf5fe58108f
elorezca <elorezca@gmail.com>
2023-10-07 17:08:47 -0400

    Practice Code

---
+++ Pratice.py
@@ -0,0 +1,25 @@
+
+print("Hello world")
+
+name = input("What is your name? ")
+
+print(f"Hello,  {name}")
+
+x = int(input("Please enter a number? "))
+y = int(input("Please enter a second number? "))
+
+print("The sum of the two numbers is: ")
+print(x+y)
+
+print("The difference of the two numbers is: ")
+print(x-y)
+
+print("The product of the two numbers is: ")
+print(x*y)
+
+print("The quotient of the two numbers is: ")
+print(x/y)
+
+age = int(input("How old are you? "))
+
+print(f"{name}, will be {age + 1} years old next year")
