4baa5b3b55af379f6efaf71fad9f5408ef90e9dd
elorezca <elorezca@gmail.com>
2023-10-07 16:55:54 -0400

    Initial commit

---
+++ area_calculator.py
@@ -0,0 +1,14 @@
+# x = int(input("What is side one of the rectrangle? "))
+# y = int(input("What is side two of the rectrangle? "))
+
+# print("The area of the rectangle is: ")
+# print(x*y)
+
+
+power = 10
+
+base = 4
+
+zero = 0
+
+print(str(base) + power * str(zero))
