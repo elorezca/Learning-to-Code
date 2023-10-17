c4b2fc503b4d10844e27119f7dedcdfa8986e591
elorezca <elorezca@gmail.com>
2023-10-16 22:39:14 -0400

    Assigment 2

---
+++ data_processing.py
@@ -0,0 +1,32 @@
+ 
+# sentence = input("Please enter a sentence you would like capitalized: ")
+
+# sentence_formatted = sentence.upper()
+
+# print(sentence_formatted)
+
+# sentence2 = input("Please type a paragrah and I will count how many words there are: ")
+
+# print(f"This sentence has",len(sentence2.split()), "words")
+
+# sentence3 = input("Please enter a string: ")
+
+# res = isinstance(sentence3, int)
+
+# print(str(res))
+
+# sentence4 = input("Please enter a sentence: ").replace("a", "o")
+
+# print(sentence4)
+
+# sentence5 = input("Please enter your name: ")
+
+# space1 = sentence5.find(' ')
+
+# newtext = sentence5[0].capitalize() + sentence5[space1+1].capitalize()
+
+# print(f"Your Initials are: {newtext}")
+
+sentence6 = input("Please enter another string: ")
+
+print(f"The length of your string is: ", (len(sentence6)))
